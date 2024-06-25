fb81f41bbb177993de509b8e1fa2d3893acfe00b
firdovsi <megatron_mega@mail.ru>
2024-06-25 20:25:45 +0400

    cartmodal

---
+++ scripts/modal.js
@@ -0,0 +1,19 @@
+
+
+var cartmodal = document.getElementById("cartModal");
+var btn = document.getElementById("cartBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  cartmodal.style.display = "block";
+}
+
+span.onclick = function() {
+  cartmodal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cartmodal) {
+    cartmodal.style.display = "none";
+  }
+}
